5a6f729a7af11dd4a984994ce531a0bd926e8558
Anchit <anchitlahkar0202@gmail.com>
2022-07-03 12:36:49 +0530

    03.07.2022

--- public/script.js
+++ public/script.js
@@ -1,15 +1,12 @@
-const { PeerServer } = require("peer")
+const socket = io("/");
 
-const socket = io("/")
-
-var peer = new PeerServer(undefined, {
+var peer = new Peer(undefined, {
     path: "/peerjs",
     host: "/",
-    port: "443"
-})
+    port: "443",
+});
 
-
-const user = prompt("Enter your name")
+const user = prompt("Enter your name");
 
 $(function () {
     $("#show_chat").click(function () {
@@ -17,7 +14,6 @@ $(function () {
         $(".right-window").css("display", "block")
         $(".header_back").css("display", "block")
     })
-
     $(".header_back").click(function () {
         $(".left-window").css("display", "block")
         $(".right-window").css("display", "none")
@@ -26,19 +22,23 @@ $(function () {
 
     $("#send").click(function () {
         if ($("#chat_message").val().length !== 0) {
-            socket.emit("message", $("#chat_message").val())
-            $("#chat_message").val("")
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
         }
     })
 
     $("#chat_message").keydown(function (e) {
         if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
-            socket.emit("message", $("#chat_message").val())
-            $("#chat_message").val("")
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
         }
     })
 
 })
+
+peer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id, user);
+});
 
 socket.on("createMessage", (message, userName) => {
     $(".messages").append(`
